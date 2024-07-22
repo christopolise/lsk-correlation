8e204127ea52f942d617b7d3774d0c6d1673278d
Chris Kitras <chkitras@gmail.com>
2024-07-22 10:52:57 -0600

    Broken, but useful/?

--- philsversion.py
+++ philsversion.py
@@ -8,6 +8,7 @@ import sys
 from scipy import signal, stats
 import math as m
 
+
 # This is the class that WLSK uses to store a "bucket"
 class Bucket:
     def __init__(self, mil: int = None, pkts: int = None):
@@ -135,10 +136,10 @@ def new_correlate(data: list[int], word: list[int]) -> np.ndarray:
     # That requires further calculation not done here.
     return corr_data
 
+
 all_zero_percentages = []
-def zero_percentage(packets, time_center, threshold_value, ax):
-    before_window = 100
-    after_window = 2
+
+def zero_percentage(packets, time_center, threshold_value, ax, before_window=90, after_window=12):
 
     num_zeros = sum(
         1
@@ -169,30 +170,40 @@ def zero_percentage(packets, time_center, threshold_value, ax):
     return result
 
 
-def sync_threshold(packets, time_center, threshold_value, ax):
+def sync_threshold(packets=None, time_center=None, threshold_value=None, ax=None):
+    if packets is None or time_center is None or threshold_value is None:
+        raise ValueError("You must provide packets, time_center, and threshold_value")
     bit_decision = packets > threshold_value
     return np.where(bit_decision == 1)[0]
 
 
 def sync_on_zero(packets, time_center, threshold_value, ax):
-    bit_decision = packets == 0
-    return np.where(bit_decision == 1)[0]
+    # bit_decision is where the packets have 10 sequential zeros
+    bit_decision = np.array(
+        [
+            i
+            for i in range(len(packets) - threshold_value - 1)  # Ensure no out-of-bounds access
+            if all(packets[i + j] == 0 for j in range(threshold_value))
+        ]
+    )
+    return bit_decision
 
 
-def new_correlation_2(packets, times, word):
+def new_correlation_2(packets, times, word, sync_func=None, bit_func=None, sf_kwargs=None, bf_kwargs=None):
     fig, ax = plt.subplots()
     ax.scatter(times, packets, s=2)
     ax.set_title("Packets Over Time")
     ax.set_xlabel("Time (ms)")
     ax.set_ylabel("Packets")
 
-    threshold_value = 10
-    before_window = 10
-    after_window = 20
+    threshold_value = 10#10 # 10
+    before_window = 1#90 # 1
+    after_window = 101#12 # 101
 
     ax.axhline(y=threshold_value, color="r", linestyle="--", label="Threshold")
 
     for one_index in sync_threshold(packets, times, threshold_value, None):
+    # for one_index in sync_on_zero(packets, times, threshold_value, None):
         ax.vlines(times[one_index], 0, 100, color="red", linestyle="--")
         print(times[one_index])
         result = []
@@ -208,7 +219,7 @@ def new_correlation_2(packets, times, word):
             )
 
             # print(packets[possible_one_time - window : possible_one_time + window])
-            if zero_percentage(packets, possible_one_time, threshold_value, ax):
+            if zero_percentage(packets, possible_one_time, threshold_value, ax, before_window, after_window):
                 color = "black" if word[i] == 1 else "blue"
                 result.append(1)
                 ax.text(
@@ -237,6 +248,7 @@ def new_correlation_2(packets, times, word):
         ax.set_ylabel("Frequency")
 
         plt.show()
+        all_zero_percentages.clear()
 
         # print(result)
 
@@ -244,7 +256,6 @@ def new_correlation_2(packets, times, word):
         print(
             f"S + P:\t\tNumber of elements that are different between result and word: {diff_count}"
         )
-
 
         # fig.set_size_inches(17.5, 9)
         if diff_count > 10:
@@ -266,38 +277,6 @@ def new_correlation_2(packets, times, word):
         plt.show()
         exit()
         return result
-
-    # previous_one_index = None
-    # result = []
-    # for one_index in np.where(bit_decision == 1)[0]:
-    #     if previous_one_index is None:
-    #         previous_one_index = one_index
-    #         result.append(1)
-    #         print("1")
-    #         continue
-
-    #     time_diff = times[one_index] - times[previous_one_index]
-    #     print(time_diff, end=" ")
-
-    #     # Combine together packets that are too close together
-    #     if time_diff < 5:
-    #         print(", skipping")
-    #         continue
-
-    #     print("1, ", end=" ")
-    #     result.append(1)
-    #     print(f"*{round(time_diff / 102) - 1}*", end=" ")
-    #     for _ in range(round(time_diff / 102) - 1):
-    #         print("0, ", end=" ")
-    #         result.append(0)
-    #     print()
-
-    #     previous_one_index = one_index
-
-    conv: np.ndarray = np.correlate(result, word, "valid")
-    corr_data: np.ndarray = conv - conv.mean()
-
-    return corr_data
 
 
 if __name__ == "__main__":
@@ -401,79 +380,3 @@ if __name__ == "__main__":
     old_sync: np.ndarray = old_correlate(packets, sync_word)
     new_sync: np.ndarray = new_correlate(packets, sync_word)
     new_sync2: np.ndarray = new_correlation_2(packets, times, expected_data)
-
-    # print(f"Max value for old_sync: {old_sync.max()}")
-    # print(f"Max value for new_sync: {new_sync.max()}")
-
-    # result = threshold(packets, times)
-    # print(list(result))
-    # chips = packets > 10
-
-    ########################################################
-    # lambda_value = packets.mean()
-    # poisson_dist = np.random.poisson(lambda_value, len(packets))
-
-    # # Compute the CDF for packets data
-    # packets_sorted = np.sort(packets)
-    # packets_cdf = np.arange(1, len(packets_sorted) + 1) / len(packets_sorted)
-
-    # # Compute the CDF for Poisson distribution
-    # poisson_sorted = np.sort(poisson_dist)
-    # poisson_cdf = np.arange(1, len(poisson_sorted) + 1) / len(poisson_sorted)
-
-    # plt.figure()
-    # plt.hist(packets, bins=50, alpha=0.5, label="Packets Data")
-    # plt.hist(poisson_dist, bins=50, alpha=0.5, label="Poisson Distribution")
-    # plt.title("Histogram of Packets Data vs Poisson Distribution")
-    # plt.xlabel("Number of Packets")
-    # plt.ylabel("Frequency")
-    # plt.legend()
-    # plt.grid(True)
-    # plt.show()
-
-    # plt.figure()
-    # plt.plot(packets_sorted, packets_cdf, label="Packets Data CDF")
-    # plt.plot(poisson_sorted, poisson_cdf, label="Poisson Distribution CDF")
-    # plt.title("CDF of Packets Data vs Poisson Distribution")
-    # plt.xlabel("Number of Packets")
-    # plt.ylabel("CDF")
-    # plt.legend()
-    # plt.grid(True)
-    # plt.show()
-    # exit()
-    ########################################################
-
-    # NOTE: Graph the results. this is just the raw data
-    NUM_GRAPHS = 5
-
-    fig: Figure
-    ax: list[Axes]
-    fig, ax = plt.subplots(NUM_GRAPHS)
-
-    ax[0].scatter(times, packets, s=2)
-    ax[0].set_title("Packets Per Millisecond Received")
-    ax[0].set_xlabel("time (ms)")
-    ax[0].set_ylabel("packets received")
-
-    new_data: pd.Series = pd.Series(packets)
-    ax[1].plot(new_data.rolling(window=75).var().bfill())
-
-    new_sync_word = upscale(sync_word)
-    sync_word_graph = [np.nan] * 7000 + new_sync_word
-    sync_word_graph = np.array(sync_word_graph) * 10
-    ax[1].plot(sync_word_graph)
-
-    ax[2].plot(old_sync)
-    ax[2].set_title("Correlation using old method")
-
-    ax[3].plot(new_sync)
-    ax[3].set_title("Correlation using new method")
-
-    ax[4].plot(new_sync2)
-    ax[4].set_title("Correlation using new method")
-
-    # ax[3].plot(chips)
-    # ax[3].set_title("Bit Decision")
-
-    plt.tight_layout()
-    plt.show()
